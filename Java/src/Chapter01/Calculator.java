7dc133e0fa3f45d79b5d322bde2317fd5239d54b
dongho <tubus1130@gmail.com>
2023-01-04 14:15:12 +0900

    java 1,2

---
+++ Java/src/Chapter01/Calculator.java
@@ -0,0 +1,20 @@
+package Chapter01;
+
+public class Calculator {
+	
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+}
---
+++ Java/src/Chapter02/HelloGodOfJava.java
@@ -0,0 +1,8 @@
+package Chapter02;
+
+public class HelloGodOfJava {
+	public static void main(String[] args) {
+		// System.out.print("Hello God Of Java !!!");
+		System.out.print("I love this book."); // This comment will not run.
+	}
+}
