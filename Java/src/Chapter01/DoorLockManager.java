f3799ada8385cbadd87834d7fb2b795e63b3d62e
dongho <tubus1130@gmail.com>
2023-01-19 18:40:26 +0900

    review1,2

---
+++ Java/src/Chapter01/DoorLockManager.java
@@ -0,0 +1,17 @@
+package Chapter01;
+
+public class DoorLockManager {
+	String currentPassword;
+	
+	public boolean checkPassword(String password) {
+		return true;
+	}
+	
+	public void setPassword(String password) {
+		
+	}
+	
+	public void resetPassword() {
+		
+	}
+}
