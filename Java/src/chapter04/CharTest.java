9f6c2decbd046d1cb0131e85e26c81c60ec966d5
dongho <tubus1130@gmail.com>
2023-01-25 13:54:15 +0900

    java chap04 review

---
+++ Java/src/chapter04/CharTest.java
@@ -0,0 +1,16 @@
+package chapter04;
+
+public class CharTest {
+	public static void main(String[] args) {
+		char a = 'a';
+		int b = 'a';
+		char c = 97;
+		int d = 97;
+		char e = (char)d;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
